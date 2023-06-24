41afa8324e4fc31d342fccd61749a8b7b1b1d161
lovegupta112 <loveguptaofficials@gmail.com>
2023-06-25 00:11:44 +0530

    updated

--- script.js
+++ script.js
@@ -1,106 +1,98 @@
+let signupBtn = document.getElementById("signup-btn");
+let alertDiv = document.querySelector(".alert-message");
+let alertMessage = document.createElement("p");
+let userInfo = {};
 
-let signupBtn=document.getElementById('signup-btn');
-let alertDiv=document.querySelector('.alert-message');
-let alertMessage=document.createElement('p');
-let userInfo={};
+//if user havent logout from his/her account (if login credentails are present in localStorage) so we will redirect to profile page
 
-
-//if user havent logout from his/her account (if login credentails are present in localStorage) so we will redirect to profile page 
-
-window.addEventListener('load',()=>{
-    const userCredentails=JSON.parse(localStorage.getItem('userInfo'));
-    if(userCredentails && userCredentails.access_token){
-        window.open('profile.html','_self');
-    }
-})
-
-
+window.addEventListener("load", () => {
+  const userCredentails = JSON.parse(localStorage.getItem("userInfo"));
+  if (userCredentails && userCredentails.access_token) {
+    window.open("profile.html", "_self");
+  }
+});
 
 //----signup functionality-------------
-signupBtn.addEventListener('click',(event)=>{
-     event.preventDefault();
-     console.log("signup function called ...");
-    let inputs=document.getElementsByTagName('input');
+signupBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("signup function called ...");
+  let inputs = document.getElementsByTagName("input");
 
-    // console.log(inputs);
-    let fullName=inputs[0].value;
-    let email=inputs[1].value;
-    let password=inputs[2].value;
-    let confirmPassword=inputs[3].value;
-    alertDiv.innerHTML="";
-    if(fullName && email && password && confirmPassword){
- 
-        if(validateEmail(email)){
-        if( password===confirmPassword){
-        console.log('Successfully Signed Up!');
-        alertMessage.innerText='Successfully Signed Up!';
-        alertMessage.className='success';
+  // console.log(inputs);
+  let fullName = inputs[0].value;
+  let email = inputs[1].value;
+  let password = inputs[2].value;
+  let confirmPassword = inputs[3].value;
+  alertDiv.innerHTML = "";
+  if (fullName && email && password && confirmPassword) {
+    if (validateEmail(email)) {
+      if (password === confirmPassword) {
+        console.log("Successfully Signed Up!");
+        alertMessage.innerText = "Successfully Signed Up!";
+        alertMessage.className = "success";
         alertDiv.append(alertMessage);
-        userInfo={
-            fullName:fullName,
-            email:email,
-            password:password,
-            access_token:genrateRandomToken()
-        }
+        userInfo = {
+          fullName: fullName,
+          email: email,
+          password: password,
+          access_token: genrateRandomToken(),
+        };
         navigateToProfilePage();
-        }
-        else {
-        console.log('please enter same password ');
-        alertMessage.innerText='please enter same password!';
-        alertMessage.className='error';
+      } else {
+        console.log("please enter same password ");
+        alertMessage.innerText = "please enter same password!";
+        alertMessage.className = "error";
         alertDiv.append(alertMessage);
-        }
-     }
+      }
     }
-    else {
-        console.log('Error : All the fields are mandatory');
-        alertMessage.innerText='Error : All the fields are mandatory';
-        alertMessage.className='error';
-        alertDiv.append(alertMessage);
-    }
-})
+  } else {
+    console.log("Error : All the fields are mandatory");
+    alertMessage.innerText = "Error : All the fields are mandatory";
+    alertMessage.className = "error";
+    alertDiv.append(alertMessage);
+  }
+});
 
 // validating email ----
 
-function validateEmail(email){
-    console.log('validaton')
-    if(!email.includes('@') || email.indexOf('@')<1){
-        alertMessage.innerText='please enter email followed by @';
-        alertMessage.className='error';
-        alertDiv.innerHTML="";
-        alertDiv.append(alertMessage);
-      return false;
-    }
-    else if ((email.charAt(email.length-4)!=='.') && (email.charAt(email.length-3)!=='.')){
-        alertMessage.innerText="Please enter email followed by '.' like '.com' or '.in";
-        alertMessage.className='error';
-        alertDiv.innerHTML="";
-        alertDiv.append(alertMessage);
-        return false;
-    }
-    else {
-        return true;
-    }
+function validateEmail(email) {
+  console.log("validaton");
+  if (!email.includes("@") || email.indexOf("@") < 1) {
+    alertMessage.innerText = "please enter email followed by @";
+    alertMessage.className = "error";
+    alertDiv.innerHTML = "";
+    alertDiv.append(alertMessage);
+    return false;
+  } else if (
+    email.charAt(email.length - 4) !== "." &&
+    email.charAt(email.length - 3) !== "."
+  ) {
+    alertMessage.innerText =
+      "Please enter email followed by '.' like '.com' or '.in";
+    alertMessage.className = "error";
+    alertDiv.innerHTML = "";
+    alertDiv.append(alertMessage);
+    return false;
+  } else {
+    return true;
+  }
 }
 
+//genrate token of  random 16 byte of string
 
-//genrate token of  random 16 byte of string 
+function genrateRandomToken() {
+  const Strings =
+    "@#$ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-function genrateRandomToken(){
-    const Strings='@#$ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-
-    let randomToken='';
-    for(let i=1;i<=16;i++){
-        randomToken +=Strings[Math.floor(Math.random()*17)];
-    }
-    return randomToken;
+  let randomToken = "";
+  for (let i = 1; i <= 16; i++) {
+    randomToken += Strings[Math.floor(Math.random() * 17)];
+  }
+  return randomToken;
 }
-
 
 // --on click on signup it will redirect to profile page--------
-function navigateToProfilePage(){
-    localStorage.setItem('userInfo',JSON.stringify(userInfo));
-     window.open('profile.html','_self');
+function navigateToProfilePage() {
+  localStorage.setItem("userInfo", JSON.stringify(userInfo));
+  window.open("profile.html", "_self");
 }
-
-
